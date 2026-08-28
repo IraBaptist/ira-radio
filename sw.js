ce610600de1b5eadfdde8752ad1abd9bc8bf7e03
IraBaptist <ben@fbcira.com>
2026-08-28 10:47:41 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ira-radio-v1.1.0';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./public.html','./public.css','./public.js','./config.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
