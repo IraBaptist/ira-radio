b6722404570376d50d0d973e5451b34439cffd6a
IraBaptist <ben@fbcira.com>
2026-08-28 15:21:15 -0500

    Connect Public Game Center.

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // After deploying Code.gs as a Google Apps Script Web App, paste the /exec URL here.
-window.IRA_PUBLIC_API_URL = '';
+window.IRA_PUBLIC_API_URL = 'https://script.google.com/macros/s/AKfycbzT8ryDNedy-NBGip6PrZc7lS0uL5Ojtqu5Qvvz4PNEdS-7GlAkt0f3JQTWcj_UL_CQ/exec';
